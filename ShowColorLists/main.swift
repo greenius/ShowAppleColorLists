f7a549a674f6a3e47ee46bff10ba45dcc48a56fb
Steven Green <steven@greenius.co.uk>
2018-07-05 16:22:27 +0100

    Initial Commit

---
+++ ShowColorLists/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ShowColorLists
+//
+//  Created by Steven W Green on 05/07/2018.
+//  Copyright © 2018 Greenius Limited. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
