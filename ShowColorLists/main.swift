f708102592c0c66f6bbe83e4c7623f2e870fd8e5
Steven Green <steven@greenius.co.uk>
2018-07-05 17:02:57 +0100

    Show Apple and Crayons Color lists

--- ShowColorLists/main.swift
+++ ShowColorLists/main.swift
@@ -7,6 +7,30 @@
 //
 
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+func floatToHex(f: CGFloat) -> String {
+  return String(format:"%02x", Int(f * 255.99999999))
+}
 
+func showColorList(list: NSColorList) {
+  let listName : String = list.name!.rawValue
+  print(listName)
+  for colName in list.allKeys
+  {
+    let col = list.color(withKey: colName)?.usingColorSpace(NSColorSpace.genericRGB)
+    var (r,g,b,a) = (CGFloat(0),CGFloat(0),CGFloat(0),CGFloat(0))
+    col?.getRed(&r, green: &g, blue: &b, alpha: &a)
+    let colorName : String = colName.rawValue
+    print(String(format:"%@%@%@%@ %@", floatToHex(f: r), floatToHex(f: g), floatToHex(f: b), floatToHex(f: a), colorName))
+  }
+}
+
+//let colorLists = NSColorList.availableColorLists
+//for list in colorLists
+//{
+//  showColorList(list: list)
+//}
+
+showColorList(list: NSColorList(named: NSColorList.Name(rawValue: "Apple"))!)
+showColorList(list: NSColorList(named: NSColorList.Name(rawValue: "Crayons"))!)
